68671ce919fb1b5bea0edff0b6518a98c12f4b69
Bianca Marisse Garcia <70173239+BiancaMGarcia@users.noreply.github.com>
2026-08-14 23:42:24 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "my-usual-v3-1";
+const CACHE = "my-usual-v3-2";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", event => {
