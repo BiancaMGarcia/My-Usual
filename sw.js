5eb4232b280a33cb5c0d8a1fb80017507a3e80fd
Bianca Marisse Garcia <70173239+BiancaMGarcia@users.noreply.github.com>
2026-08-16 18:01:07 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "my-usual-v49-item-card-spacing";
-const ASSETS = ["./", "./index.html", "./styles.css?v=49", "./app.js?v=49", "./manifest.webmanifest?v=49", "./icon-192.png?v=49", "./icon-512.png?v=49", "./avatar-husky-blue.png?v=49", "./avatar-brown-peach.png?v=49", "./avatar-samoyed-lavender.png?v=49", "./avatar-white-mint.png?v=49", "./avatar-spitz-pink.png?v=49", "./avatar-shepherd-yellow.png?v=49"];
+const CACHE = "my-usual-v50-sticky-popup-controls";
+const ASSETS = ["./", "./index.html", "./styles.css?v=50", "./app.js?v=50", "./manifest.webmanifest?v=50", "./icon-192.png?v=50", "./icon-512.png?v=50", "./avatar-husky-blue.png?v=50", "./avatar-brown-peach.png?v=50", "./avatar-samoyed-lavender.png?v=50", "./avatar-white-mint.png?v=50", "./avatar-spitz-pink.png?v=50", "./avatar-shepherd-yellow.png?v=50"];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
